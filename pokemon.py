1f2b62211162b0cb722d1b641c28bb75760f6d64
isobel-p <isobelpang@gmail.com>
2024-06-19 19:59:09 +0100

    Add OR logic operator

--- pokemon.py
+++ pokemon.py
@@ -7,11 +7,15 @@ while True:
     search = input("Enter a letter or phrase to search for.")
     print(f'Search results for \"{search}\": ')
     start = time.time()
-    count = 0
-    for i in text:
-        if search in i.lower():
-            print(i)
-            count += 1
+    if " OR " in search:
+        searches = search.split(" OR ")
+    for i in searches:
+        count = 0
+        for j in text:
+            if i in j.lower():
+                print(j)
+                count += 1
+                text.remove(j)
     end = time.time()
     duration = end - start
     print(f'----------\n{count} search results found in {duration} seconds\n')
