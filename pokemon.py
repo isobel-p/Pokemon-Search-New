3b037e145561beac3b1f9e0200f7452df348378d
isobel-p <isobelpang@gmail.com>
2024-06-19 20:12:22 +0100

    Update "AND" operator

--- pokemon.py
+++ pokemon.py
@@ -7,13 +7,19 @@ while True:
     search = input("Enter a letter or phrase to search for.")
     print(f'Search results for \"{search}\": ')
     start = time.time()
+    count = 0
     if " OR " in search:
         searches = search.split(" OR ")
-    for i in searches:
-        count = 0
-        for j in text:
-            if i in j.lower():
-                print(j)
+        for i in searches:
+            for j in text:
+                if i in j.lower():
+                    print(j)
+                    count += 1
+                    text.remove(j)
+    else:
+        for i in text:
+            if search in i.lower():
+                print(i)
                 count += 1
                 text.remove(j)
     end = time.time()
